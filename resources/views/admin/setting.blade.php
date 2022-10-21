16190597256e825c28713a3ee9f535f96db9dfb2
kerelkh <kerelkh@gmail.com>
2022-10-21 14:10:14 +0700

    Responsive Admin Setting

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -15,7 +15,7 @@
                 @if(session('update-profile-error'))
                     <x-message-notice type="error" :value="session('update-profile-error')"></x-message-notice>
                 @endif
-                <div class="flex items-center mb-2">
+                <div class="flex flex-col sm:flex-row items-stretch sm:items-center mb-2">
                     <label for="username" class="flex-1">Username</label>
                     <div class="flex-1 flex flex-col items-stretch">
                         <input type="text" name="username" id="username" value="{{ Auth::user()->username }}" autocomplete="off" required class="px-2 py-1 rounded-lg outline-none focus:ring-1 focus:ring-blue-400 bg-gray-200 focus:bg-white">
@@ -24,7 +24,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="flex items-center mb-2">
+                <div class="flex flex-col sm:flex-row items-stretch sm:items-center mb-2">
                     <label for="name" class="flex-1">Name</label>
                     <div class="flex-1 flex flex-col items-stretch">
                         <input type="text" name="name" id="name" value="{{ Auth::user()->name }}" autocomplete="off" required class="px-2 py-1 rounded-lg outline-none focus:ring-1 focus:ring-blue-400 bg-gray-200 focus:bg-white">
@@ -33,7 +33,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="flex items-center mb-2">
+                <div class="flex flex-col sm:flex-row items-stretch sm:items-center mb-2">
                     <label for="email" class="flex-1">Email</label>
                     <div class="flex-1 flex flex-col items-stretch">
                         <input type="email" name="email" id="email" value="{{ Auth::user()->email }}" autocomplete="off" required class="px-2 py-1 rounded-lg outline-none focus:ring-1 focus:ring-blue-400 bg-gray-200 focus:bg-white">
@@ -42,7 +42,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="flex items-center mb-2 mt-8">
+                <div class="flex flex-col sm:flex-row items-stretch sm:items-center mb-2 mt-8">
                     <label for="password" class="flex-1">Current Password</label>
                     <div class="flex-1 flex flex-col items-stretch">
                         <input type="password" name="password" id="password" autocomplete="off" required class="px-2 py-1 rounded-lg outline-none focus:ring-1 focus:ring-blue-400 bg-gray-200 focus:bg-white">
@@ -52,9 +52,9 @@
                     </div>
                 </div>
                 <div class="flex items-center">
-                    <div class="flex-1 hiden"></div>
+                    <div class="flex-1 hidden"></div>
                     <div class="flex-1">
-                        <button type="submit" class="p-2 bg-blue-400 text-gray-50 rounded-xl hover:bg-blue-600">Update</button>
+                        <button type="submit" class="mt-5 sm:mt-2 p-2 bg-blue-400 text-gray-50 rounded-xl hover:bg-blue-600">Update</button>
                     </div>
                 </div>
             </div>
@@ -72,12 +72,12 @@
                 @if(session('update-banner-error'))
                 <x-message-notice type="error" value="{{ session('update-banner-error') }}"></x-message-notice>
                 @endif
-                <form action="/setting/update/banner" method="POST" enctype="multipart/form-data" class="flex gap-3">
+                <form action="/setting/update/banner" method="POST" enctype="multipart/form-data" class="flex flex-col sm:flex-row gap-3">
                     @csrf
                     @if($datas['landing-banner']->value != '' || $datas['landing-banner']->value != NULL)
                     <img src="{{ asset('storage/' . $datas['landing-banner']->value) }}" alt="Banner temporary" id="img-temp" class="w-1/2 aspect-video">
                     @else
-                    <img src="{{ asset('images/image-placeholder.png') }}" alt="Banner temporary" id="img-temp" class="w-1/2">
+                    <img src="{{ asset('images/image-placeholder.png') }}" alt="Banner temporary" id="img-temp" class="w-full sm:w-1/2">
                     @endif
                     <div class="flex flex-col justify-between items-start gap-5">
                         <input
